77a344dc0228e5581260e1b238483c300afaa953
ppublic <p.public@protonmail.com>
2020-05-26 11:42:38 -0500

    Self-Organizing Feature Map

--- Learning/unsupervised.py
+++ Learning/unsupervised.py
@@ -92,3 +92,80 @@ def kmeansnet(inputs, kClusters, **kwargs):
     train()
 
     return classify
+
+
+def som(data, map_shape, **kwargs):
+
+    def forward(inputs):
+        if isinstance(inputs, str) and inputs == "getMap":
+            return som_map
+        activations = np.sum((np.transpose(np.tile(inputs, (x * y, 1))) - weights)**2, axis=0)
+        best = np.argmin(activations)
+        return best
+
+    def train():
+        nonlocal weights, eta_b, eta_neighbour, neighbourSize
+        eta_binit, eta_ninit, nSizeInit = eta_b, eta_neighbour, neighbourSize
+
+        for i in range(nIterations):
+            for iData in range(nData):
+                best = forward(data[iData, :])
+                weights[:, best] += eta_b * (data[iData, :] - weights[:, best])
+
+                neighbours = np.where(mapDistances[best, :] <= neighbourSize, 1, 0)
+                neighbours[best] = 0
+                weights += eta_neighbour  * neighbours * np.transpose((data[iData, :] - weights.T))
+
+            eta_b = eta_binit * np.power(eta_bfinal/eta_binit, float(i)/nIterations)
+            eta_neighbour = eta_ninit * np.power(eta_nfinal/eta_ninit, float(i)/nIterations)
+            neighbourSize = nSizeInit * np.power(neighbourSizeFinal/nSizeInit, float(i)/nIterations)
+
+    eta_b = kwargs.get("eta_b", 0.3)
+    eta_neighbour = kwargs.get("eta_n", 0.1)
+    eta_bfinal = kwargs.get("eta_bfinal", 0.03)
+    eta_nfinal = kwargs.get("eta_nfinal", 0.01)
+    neighbourSize = kwargs.get("neighbourSize", 0.5)
+    neighbourSizeFinal = kwargs.get("neighbourSizeFinal", 0.05)
+    boundaries = bool(kwargs.get("boundaries", False))
+    nIterations = kwargs.get("nIterations", 100)
+
+    x = map_shape[0]
+    y = map_shape[1]
+
+    nData = data.shape[0]
+
+    som_map = np.mgrid[0:1:np.complex(0, x), 0:1:np.complex(0, y)]
+    som_map = som_map.reshape(2, x * y)
+
+    weights = (np.random.rand(data.shape[1], x * y) - 0.5) * 2
+
+    mapDistances =np.zeros((x * y, x * y))
+
+    if boundaries:
+        for i in range(x * y):
+            for j in range(i+1, x * y):
+                xdist = np.min( (som_map[0,i]-som_map[0,j])**2, \
+                                (som_map[0,i]+1+1./x-som_map[0,j])**2, \
+                                (som_map[0,i]-1-1./x-som_map[0,j])**2, \
+                                (som_map[0,i]-som_map[0,j]+1+1./x)**2, \
+                                (som_map[0,i]-som_map[0,j]-1-1./x)**2
+                )
+                ydist = np.min( (som_map[1,i]-som_map[1,j])**2, \
+                                (som_map[1,i]+1+1./y-som_map[1,j])**2, \
+                                (som_map[1,i]-1-1./y-som_map[1,j])**2, \
+                                (som_map[1,i]-som_map[1,j]+1+1./y)**2, \
+                                (som_map[1,i]-som_map[1,j]-1-1./y)**2
+                )
+                mapDistances[i, j] = np.sqrt(xdist + ydist)
+                mapDistances[j, i] = mapDistances[i, j]
+    else:
+        for i in range(x * y):
+            for j in range(i+1, x * y):
+                mapDistances[i, j] = np.sqrt( (som_map[0, i] - som_map[0, j])**2 + \
+                                              (som_map[1, i] - som_map[1, j])**2
+                                              )
+                mapDistances[j, i] = mapDistances[i, j]
+
+    train()
+
+    return forward
--- unsupervised_test.py
+++ unsupervised_test.py
@@ -1,28 +1,31 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from Learning.unsupervised import kmeans, kmeansnet
+from Learning.unsupervised import kmeans, kmeansnet, som
 
 def read_iris(filepath):
     iris = np.loadtxt(filepath, delimiter=',')
-    iris[:,:4] = iris[:,:4]-iris[:,:4].mean(axis=0)
-    imax = np.concatenate((iris.max(axis=0)*np.ones((1,5)),iris.min(axis=0)*np.ones((1,5))),axis=0).max(axis=0)
-    iris[:,:4] = iris[:,:4]/imax[:4]
+    iris[:, :4] = iris[:, :4]-iris[:, :4].mean(axis=0)
+    imax = np.concatenate((iris.max(axis=0)*np.ones((1, 5)),
+                           iris.min(axis=0)*np.ones((1, 5))), axis=0).max(axis=0)
+    iris[:, :4] = iris[:, :4]/imax[:4]
 
-    target = iris[:,4]
+    target = iris[:, 4]
 
     order = [*range(np.shape(iris)[0])]
     np.random.shuffle(order)
-    iris = iris[order,:]
+    iris = iris[order, :]
     target = target[order]
 
     return iris, target
+
 
 def create_mapping(predictions, targets):
     mapping = np.zeros((3, 3))
     for i in range(len(predictions)):
         mapping[predictions[i], targets[i]] += 1
     return mapping
+
 
 if __name__ == "__main__":
     data, targets = read_iris(os.path.join(os.getcwd(), "Data/iris_proc.data"))
@@ -31,6 +34,11 @@ if __name__ == "__main__":
     x_out = targets[::2]
     y_in = data[1::2, 0:4]
     y_out = targets[1::2].astype(int)
+
+    train = x_in
+    traint = x_out
+    test = y_in
+    testt = y_out
 
     cluster = kmeans(x_in, 3, average="mean")
     predict = cluster(y_in)
@@ -42,5 +50,43 @@ if __name__ == "__main__":
     print(f"KMEANS NET: {predict_net[:, 0]}")
     print(f"ACTUAL    : {y_out}")
 
-    print(f"KMEANS DIFFS COUNTS     : \n{create_mapping(predict[:, 0], y_out)}")
-    print(f"KMEANS NET DIFFS COUNTS : \n{create_mapping(predict_net[:, 0], y_out)}")
+    print(
+        f"KMEANS DIFFS COUNTS     : \n{create_mapping(predict[:, 0], y_out)}")
+    print(
+        f"KMEANS NET DIFFS COUNTS : \n{create_mapping(predict_net[:, 0], y_out)}")
+
+    forward = som(x_in, (6, 6), nIterations=400)
+    som_map = forward("getMap")
+    mapped = np.zeros(y_in.shape[0], dtype=int)
+
+    for i in range(len(y_in)):
+        mapped[i] = forward(x_in[i, :])
+    plt.figure()
+    plt.title("Trained SOM Map")
+    plt.plot(som_map[0, :], som_map[1, :], 'k.', ms=15)
+    where = np.argwhere(x_out == 0).flatten()
+    plt.plot(som_map[0, mapped[where]], som_map[1, mapped[where]], 'rs', ms=30, label="0")
+    where = np.argwhere(x_out == 1).flatten()
+    plt.plot(som_map[0, mapped[where]], som_map[1, mapped[where]], 'gv', ms=30, label="1")
+    where = np.argwhere(x_out == 2).flatten()
+    plt.plot(som_map[0, mapped[where]], som_map[1, mapped[where]], 'b^', ms=30, label="2")
+    plt.axis([-0.1, 1.1, -0.1, 1.1])
+    plt.axis('off')
+    plt.show(block=False)
+
+    mapped = np.zeros(y_in.shape[0], dtype=int)
+    for i in range(len(y_in)):
+        mapped[i] = forward(y_in[i, :])
+
+    plt.figure()
+    plt.title("Tested SOM Map")
+    plt.plot(som_map[0, :], som_map[1, :], 'k.', ms=15)
+    where = np.argwhere(y_out == 0).flatten()
+    plt.plot(som_map[0, mapped[where]], som_map[1, mapped[where]], 'rs', ms=30, label="0")
+    where = np.argwhere(y_out == 1).flatten()
+    plt.plot(som_map[0, mapped[where]], som_map[1, mapped[where]], 'gv', ms=30, label="1")
+    where = np.argwhere(y_out == 2).flatten()
+    plt.plot(som_map[0, mapped[where]], som_map[1, mapped[where]], 'b^', ms=30, label="2")
+    plt.axis([-0.1, 1.1, -0.1, 1.1])
+    plt.axis('off')
+    plt.show(block=True)
